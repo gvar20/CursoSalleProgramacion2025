3c7f7fb29eaa99dc6e7a4d11a6a77ccdd4f2a2e2
gvar20 <55330786+gvar20@users.noreply.github.com>
2025-01-20 10:16:16 -0500

    Update MostrarCeroUnoHilos.java

--- Clases-Semana03/MostrarCeroUnoHilos.java
+++ Clases-Semana03/MostrarCeroUnoHilos.java
@@ -1,6 +1,7 @@
 package creacion;
 
 public class MostrarCeroUnoHilos {
+	/*Existirian en este momento 3 hilos el h1, h2 y el main (este también es otro hilo)*/
 	public static void main(String[] args) {
 		HiloCero h1 = new HiloCero();
 		HiloUno h2 = new HiloUno();
